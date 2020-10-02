de6ebbb2e72935c8fe808fabe5b4634cbe23d8e0
AndreaMorales-gif <andreamoralesmarin13@gmail.com>
2020-10-02 17:47:47 -0500

    Actualizar archivos

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ app.use(express.json());
 const routes = require('./routes/routes');
 app.use('/api/', routes);
 
-app.use('/static', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../index2.html'))
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/statics', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index2.html'))
 })
+
 
 app.listen(3000, () => {
     console.log('server started');
